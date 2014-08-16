4e52c963159b837fc1c9801b6439e783ed339b91
sebiol <s.hoehle@gmail.com>
2014-08-16 14:20:46 +0200

    removed config file with sensitive data from index

--- spec/dummy/config/initializers/giro_checkout.rb
+++
@@ -1,5 +0,0 @@
-GiroCheckout.configure do |config|
-  merchantId = '3609667'
-  giropay = { 'projectId' => '8878', 'projectSecret' => 'giroTest' }
-  message_urls = { 'bankstatus' => 'https://payment.girosolution.de/girocheckout/api/v2/giropay/bankstatus' } 
-end
