1907f7bdf8d5b785dff83bd6501f1de581669d68
Sebastian Höhle <mail@sebiol.de>
2014-08-27 09:53:36 +0200

    added factorygirl to spechelper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-#require 'factory_girl_rails'
+require 'factory_girl'
 require 'webmock/rspec'
+require 'lib/giro_checkout.rb'
+require 'app/models/giro_checkout/transaction'
+require 'factories/transactions'
 
 Rails.backtrace_cleaner.remove_silencers!
 WebMock.disable_net_connect!( :allow_localhost => true )
@@ -17,4 +20,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
+  config.include FactoryGirl::Syntax::Methods
 end
