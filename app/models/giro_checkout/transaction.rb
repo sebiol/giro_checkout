3e74f213b1082b6f93319b86c79963974ec5948a
sebiol <mail@sebiol.de>
2014-08-30 20:49:17 +0200

    Status code defined as constants on Transaction

--- app/models/giro_checkout/transaction.rb
+++ app/models/giro_checkout/transaction.rb
@@ -2,6 +2,17 @@ module GiroCheckout
   class Transaction < ActiveRecord::Base
     attr_accessible :amount, :currency, :project_id, :gcPSPTransactionID, :gcTransactionID, :purpose, :description, :status
 
+    #Status codes
+    Initialized   = 1001
+    Started       = 1002
+    Pending       = 1003
+
+    Successful    = 2000
+
+    Aborted       = 4000
+    Failed        = 4000
+    
+
     def rel_attributes
       result = Hash.new(nil)
       result['merchantTxId'] = self.id
