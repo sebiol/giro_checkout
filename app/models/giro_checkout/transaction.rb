30b0b0f348609914ff3aa4117dff4b71d851d528
Sebastian Höhle <mail@sebiol.de>
2014-12-29 14:42:41 +0100

    skeleton methods on Transaction object

--- app/models/giro_checkout/transaction.rb
+++ app/models/giro_checkout/transaction.rb
@@ -12,6 +12,13 @@ module GiroCheckout
     Aborted       = 4001
     Failed        = 4002
     
+    def update_status()
+      raise "not implemented yet"
+    end
+
+    def pay(payment_data)
+      raise "not implemented yet"
+    end
 
     def rel_attributes
       result = Hash.new(nil)
