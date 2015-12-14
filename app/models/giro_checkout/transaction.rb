b25d2c4ff8cb1bc7a4272dbbfa44c4d6c744b6f2
sebiol <mail@sebiol.de>
2015-12-14 18:24:04 +0100

    [Fix] length instead of count for length of BIC
    The method count posed some issues. Therefor change to the length method
    to compare the size of BIC to specification

--- app/models/giro_checkout/transaction.rb
+++ app/models/giro_checkout/transaction.rb
@@ -36,9 +36,9 @@ module GiroCheckout
         Rails.logger.info 'create message'
         msg = GcPaypaltransactionstartMessage.new( self )
       elsif payment_data.keys[0] == 'giropay'
-        return :invalid_BIC if payment_data['giropay']['BIC'].count < 8
-        return :invalid_BIC if payment_data['giropay']['BIC'].count > 11
         return :no_BIC if payment_data['giropay']['BIC'].nil?
+        return :invalid_BIC if payment_data['giropay']['BIC'].length < 8
+        return :invalid_BIC if payment_data['giropay']['BIC'].length > 11
 
         Rails.logger.info 'start giropay transaction'
         Rails.logger.info 'create message'
