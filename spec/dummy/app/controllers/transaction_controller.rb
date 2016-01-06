88c7c671e4ebe62687d135644e7d2d0402bb4c3f
sebiol <mail@sebiol.de>
2016-01-06 19:10:19 +0100

    Dummy App switched to new API

--- spec/dummy/app/controllers/transaction_controller.rb
+++ spec/dummy/app/controllers/transaction_controller.rb
@@ -9,18 +9,17 @@ class TransactionController < ApplicationController
     logger.info params['method']
     logger.info params['payment'].inspect
 
-    transaction_data['project_id'] = GiroCheckout.project_id(params['method'])
-
     transaction = GiroCheckout.create_transaction(transaction_data)
     logger.debug transaction
-    res = transaction.pay(params['payment'])
+    res = transaction.pay(params['payment'], params['method'])
+    logger.info res.inspect
 
     #Redirect to result url if contains url
-    if res =~ /^https:/
-      redirect_to res 
+    if res.has_key?('redirect') && res['redirect'] != nil
+      redirect_to res['redirect']
     else
       #If result dosen't contain a url redirect to form page with error
-      session[:error] = res
+      session[:error] = "#{res['rc']} - #{res['msg']}"
       redirect_to '/forms'
     end
   end
